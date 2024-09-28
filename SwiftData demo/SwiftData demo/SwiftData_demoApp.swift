c7524584eb101fe82cbf5b2cdcadf97404f9d6fe
Ricardo Casanova <ricardo.casanova@outlook.com>
2024-09-28 19:18:29 +0200

    Add initial Xcode project

---
+++ SwiftData demo/SwiftData demo/SwiftData_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftData_demoApp.swift
+//  SwiftData demo
+//
+//  Created by Ricardo Casanova on 28/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftData_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftData demo/SwiftData demoTests/SwiftData_demoTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftData_demoTests.swift
+//  SwiftData demoTests
+//
+//  Created by Ricardo Casanova on 28/9/24.
+//
+
+import Testing
+
+struct SwiftData_demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
